2367859097a3d14f73b9ee470181743ba1aa9c1e
avmm9898 <b04611002@ntu.edu.tw>
2020-01-19 14:58:39 +0800

    test vue118n

---
+++ lang/en.js
@@ -0,0 +1,32 @@
+const messages = {
+    en: {
+      message: {
+        hello: 'hello',
+        minwt:'minwt'
+      }
+    },
+    tw: {
+      message: {
+        hello: '哈囉',
+        minwt:'梅問題'
+      }
+    },
+    cn: {
+      message: {
+        hello: '哈啰',
+        minwt:'梅问题'
+      }
+    },
+    jp: {
+      message: {
+        hello: 'ハロー',
+        minwt:'メイウンディー'
+      }
+    }
+  }
+  
+  const i18n = new VueI18n({
+    locale: 'en', 
+    messages,
+  })
+  new Vue({ i18n }).$mount('#language')
