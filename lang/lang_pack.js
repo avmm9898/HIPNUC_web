14625e22c81027c5f7fc57dd58a8e744cbe08cdf
avmm9898 <b04611002@ntu.edu.tw>
2020-01-23 01:25:34 +0800

    update all trans

--- lang/lang_pack.js
+++ lang/lang_pack.js
@@ -5,37 +5,45 @@ const messages = {
       products: 'Products',
       documentation: 'Documentation',
       contact: 'Contact&Purchase',
-      language: 'Language'
+      language: 'Language',
+
+    },
+    index: {
+      intro: 'Welcome to HiPNUC',
+      intro2: 'We make professional, advanced, and tiny IMU/AHRS systems and products.',
     },
     contact: {
-      intro: 'Contact&Purchase',
+
       title1: 'Sample Requests & Supports',
       content1: 'If you need samples, bulk purchase, or other supports, please contact our office.',
       beijing: 'Beijing Head Office',
       b_address: 'Address : B01-1-1001, Fuli Yunhe No.10, Zhaodengyu St, Tongzhou, Beijing, China',
-      b_phone:'Phone (Mr.Liu) : +86 18612030435',
+      b_phone: 'Phone (Mr.Liu) : +86 18612030435',
       taobao: 'Buy on Taobao >',
       taipei: 'Taipei',
       tw_phone: 'Phone (Mr.Hsu) : +886 0908179107',
-      ruten:'Buy on Ruten >',
+      ruten: 'Buy on Ruten >',
       title2: 'Business Partner',
       content2: 'We welcome you all no matter you are researcher, student, maker or company. These are parts of our customers.'
     },
-    footer:{
-      about_title:'About HiPNUC',
-      about_content:'HiPNUC is dedicated to develope cost-effective IMU/AHRS with high quality, providing mature and advanced integrated navigation modules.',     
-      github:'Visit our github',
-      library_title:'Our Library',
-      lib1:'Digital filering with matlab',
-      lib2:'Mathematic basis of rotation',
-      lib3:'Basic concepts of 3D inertial sensors and attitude',
-      lib4:'Notes of MIT linear algebra',
-      lib5:'Basic concepts of IMU/VRU/AHRS/6-axis/9-axis',
-      opentime_title:'Business Hours',
-      opentime1:'Monday to Friday : 09:00-19:00',
-      opentime2:'Saturday : 09:00-17:00',
-      sales1:'Sales (Beijing)',
-      sales2:'Sales (Taipei)'
+    products: {
+
+    },
+    footer: {
+      about_title: 'About HiPNUC',
+      about_content: 'HiPNUC is dedicated to develope cost-effective IMU/AHRS with high quality, providing mature and advanced integrated navigation modules.',
+      github: 'Visit our github',
+      library_title: 'Our Library',
+      lib1: 'Digital filering with matlab',
+      lib2: 'Mathematic basis of rotation',
+      lib3: 'Basic concepts of 3D inertial sensors and attitude',
+      lib4: 'Notes of MIT linear algebra',
+      lib5: 'Basic concepts of IMU/VRU/AHRS/6-axis/9-axis',
+      opentime_title: 'Business Hours',
+      opentime1: 'Monday to Friday : 09:00-19:00',
+      opentime2: 'Saturday : 09:00-17:00',
+      sales1: 'Sales (Beijing)',
+      sales2: 'Sales (Taipei)'
     }
 
   },
@@ -46,40 +54,45 @@ const messages = {
       documentation: '文件中心',
       contact: '聯絡與購買',
       language: '語言',
+    },
+    index: {
+      intro: 'HiPNUC 超核電子',
+      intro2: '致力於開發高經濟效益、專業、先進、小巧的 IMU/AHRS 系統與產品。',
+    },
+    products: {
 
     },
     contact: {
-      intro: '聯絡與購買',
+
       title1: '樣本申請與銷售支援',
       content1: '如果您需要申請樣品，批量購買或其他支援與服務，請聯絡我們的辦公室。',
       beijing: '北京總公司',
       b_address: '地址 : 北京市通州區趙登禹大街富力運河十號B01-1-1001',
-      b_phone:'連絡電話 (劉先生) : +86 18612030435',
+      b_phone: '連絡電話 (劉先生) : +86 18612030435',
       taobao: '前往淘寶購買 >',
       taipei: '台灣代理',
       tw_phone: '聯絡電話 (許先生) : +886 0908179107',
-      ruten:'前往露天購買 >',   
+      ruten: '前往露天購買 >',
       title2: '合作夥伴',
       content2: '無論您是研究人員，學生，創客或是公司，我們都竭誠歡迎您。以下是使用我們產品的部分客戶。'
 
     },
-    footer:{
-      about_title:'關於超核電子',
-      about_content:'HiPNIC 超核電子致力於開發具有高成本效益的高質量IMU / AHRS。多年的低成本MEMS傳感器算法開發經驗，提供成熟和先進的集成導航模組，為客戶提供最具性價比的姿態檢測方案。',     
-      github:'前往我們的Github',
-      library_title:'圖書館',
-      lib1:'Matlab 數字濾波入門',
-      lib2:'旋轉的數學表示-四元數與歐拉角以及相互轉化',
-      lib3:'三軸慣性傳感器和姿態解算的基本概念',
-      lib4:'MIT線性代數筆記',
-      lib5:'IMU/VRU/AHRS/6軸9軸等基本概念',
-      opentime_title:'服務時間',
-      opentime1:'周一至周五 : 09:00-19:00',
-      opentime2:'周六至周日 : 09:00-17:00',
-      sales1:'銷售點 (北京)',
-      sales2:'銷售點 (台北)'
+    footer: {
+      about_title: '關於超核電子',
+      about_content: 'HiPNIC 超核電子致力於開發具有高成本效益的高質量IMU / AHRS。多年的低成本MEMS傳感器算法開發經驗，提供成熟和先進的集成導航模組，為客戶提供最具性價比的姿態檢測方案。',
+      github: '前往我們的Github',
+      library_title: '圖書館',
+      lib1: 'Matlab 數字濾波入門',
+      lib2: '旋轉的數學表示-四元數與歐拉角以及相互轉化',
+      lib3: '三軸慣性傳感器和姿態解算的基本概念',
+      lib4: 'MIT線性代數筆記',
+      lib5: 'IMU/VRU/AHRS/6軸9軸等基本概念',
+      opentime_title: '服務時間',
+      opentime1: '周一至周五 : 09:00-19:00',
+      opentime2: '周六至周日 : 09:00-17:00',
+      sales1: '銷售點 (北京)',
+      sales2: '銷售點 (台北)'
     }
-    
   },
   cn: {
     navbar: {
@@ -87,11 +100,45 @@ const messages = {
       products: '产品型录',
       documentation: '文件中心',
       contact: '联络与购买',
-      language: '语言'
+      language: '语言',
+    },
+    index: {
+      intro: 'HiPNUC 超核电子',
+      intro2: '致力于开发高经济效益、专业、先进、小巧的 IMU/AHRS 系统与产品。',
+    },
+    products: {
+
     },
     contact: {
-      
-     
+
+      title1: '样本申请与销售支援',
+      content1: '如果您需要申请样品，批量购买或其他支援与服务，请联络我们的办公室。',
+      beijing: '北京总公司',
+      b_address: '地址 : 北京市通州区赵登禹大街富力运河十号B01-1-1001',
+      b_phone: '连络电话 (刘先生) : +86 18612030435',
+      taobao: '前往淘宝购买 >',
+      taipei: '台湾代理',
+      tw_phone: '联络电话 (许先生) : +886 0908179107',
+      ruten: '前往露天购买 >',
+      title2: '合作伙伴',
+      content2: '无论您是研究人员，学生，创客或是公司，我们都竭诚欢迎您。以下是使用我们产品的部分客户。'
+
+    },
+    footer: {
+      about_title: '关于超核电子',
+      about_content: 'HiPNIC 超核电子致力于开发具有高成本效益的高质量IMU / AHRS。多年的低成本MEMS传感器算法开发经验，提供成熟和先进的集成导航模组，为客户提供最具性价比的姿态检测方案。',
+      github: '前往我们的Github',
+      library_title: '图书馆',
+      lib1: 'Matlab 数字滤波入门',
+      lib2: '旋转的数学表示-四元数与欧拉角以及相互转化',
+      lib3: '三轴惯性传感器和姿态解算的基本概念',
+      lib4: 'MIT线性代数笔记',
+      lib5: 'IMU/VRU/AHRS/6轴9轴等基本概念',
+      opentime_title: '服务时间',
+      opentime1: '周一至周五 : 09:00-19:00',
+      opentime2: '周六至周日 : 09:00-17:00',
+      sales1: '销售点 (北京)',
+      sales2: '销售点 (台北)'
     }
   },
 };
@@ -138,7 +185,9 @@ function init_lang() {
   var tmp_lang = getCookie("lang");
   if (tmp_lang == "en" || tmp_lang == "cn" || tmp_lang == "tw")
     i18n.locale = tmp_lang
-  new Vue({ i18n }).$mount('#language');
+  new Vue({ i18n }).$mount('#navbar');
+  new Vue({ i18n }).$mount('#div1');
   new Vue({ i18n }).$mount('#div2');
   new Vue({ i18n }).$mount('#div3');
+  new Vue({ i18n }).$mount('#footer');
 }
