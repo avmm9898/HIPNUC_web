9244b944a0333e6e16118ecacf5d181c22990b26
avmm9898 <b04611002@ntu.edu.tw>
2020-01-22 23:24:36 +0800

    update translation
    更新聯絡頁面中英翻譯

--- lang/lang_pack.js
+++ lang/lang_pack.js
@@ -7,8 +7,19 @@ const messages = {
       contact: 'Contact&Purchase',
       language: 'Language'
     },
-    content: {
-      title: 'Sample Requests & Supports'
+    contact: {
+      intro: 'Contact&Purchase',
+      title1: 'Sample Requests & Supports',
+      content1: 'If you need samples, bulk purchase, or other supports, please contact our office.',
+      beijing: 'Beijing Head Office',
+      b_address: 'Address : B01-1-1001, Fuli Yunhe No.10, Zhaodengyu St, Tongzhou, Beijing, China',
+      b_phone:'Phone (Mr.Liu) : +86 18612030435',
+      taobao: 'Buy on Taobao >',
+      taipei: 'Taipei',
+      tw_phone: 'Phone (Mr.Hsu) : +886 0908179107',
+      ruten:'Buy on Ruten >',
+      title2: 'Business Partner',
+      content2: 'We welcome you all no matter you are researcher, student, maker or company. These are parts of our customers.'
     }
 
   },
@@ -16,21 +27,38 @@ const messages = {
     navbar: {
       home: '首頁',
       products: '產品型錄',
-      documentation: '產品文件',
+      documentation: '文件中心',
       contact: '聯絡與購買',
-      language: '語言'
+      language: '語言',
+
+    },
+    contact: {
+      intro: '聯絡與購買',
+      title1: '樣本申請與銷售支援',
+      content1: '如果您需要申請樣品，批量購買或其他支援與服務，請聯絡我們的辦公室。',
+      beijing: '北京總公司',
+      b_address: '地址 : 北京市通州區趙登禹大街富力運河十號B01-1-1001',
+      b_phone:'連絡電話 (劉先生) : +86 18612030435',
+      taobao: '前往淘寶購買 >',
+      taipei: '台灣代理',
+      tw_phone: '聯絡電話 (許先生) : +886 0908179107',
+      ruten:'前往露天購買 >',   
+      title2: '合作夥伴',
+      content2: '無論您是研究人員，學生，創客或是公司，我們都竭誠歡迎您。以下是使用我們產品的部分客戶。'
+
     }
   },
   cn: {
     navbar: {
       home: '首页',
       products: '产品型录',
-      documentation: '产品文件',
+      documentation: '文件中心',
       contact: '联络与购买',
       language: '语言'
     },
-    content: {
-      title: '樣本申請與銷售支援'
+    contact: {
+      
+     
     }
   },
 };
@@ -77,7 +105,7 @@ function init_lang() {
   var tmp_lang = getCookie("lang");
   if (tmp_lang == "en" || tmp_lang == "cn" || tmp_lang == "tw")
     i18n.locale = tmp_lang
-  new Vue({ i18n }).$mount('#navbar_lang');
+  new Vue({ i18n }).$mount('#language');
   new Vue({ i18n }).$mount('#div2');
-
+  new Vue({ i18n }).$mount('#div3');
 }
--- mkdocs/docs/index.md
+++ mkdocs/docs/index.md
@@ -1,11 +1,33 @@
 # Introduction/簡介
-Check the multi-language user guides of our products.
-We've supported those languages:
+Welcome to our documentation center. We've prepared multi-language user guides of our products for you.
 
-歡迎查看我們產品的多語言用戶指南。
-我們已支持以下語言：
+Products list:
+
+- Wireless IMU system series: 
+	- Hi221/Hi221GW
+- IMU module series:
+	- Hi226
+	- Hi229
+
+ 
+ 
+
+歡迎進入文件中心，我們提供了多語言的產品用戶指南。
+
+現有產品清單:
+
+- 無線慣性測量系統系列: 
+	- Hi221/Hi221GW
+- 慣性測量儀模組系列:
+	- Hi226
+	- Hi229
 
 
+## Languages supported / 語言支援
+
+We've supported those languages for our user guides:
+
+並且已支持以下語言的產品使用手冊：
 
 - 简体版
 - 繁體版
